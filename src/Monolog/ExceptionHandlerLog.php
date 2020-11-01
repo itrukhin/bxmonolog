aca42657e67113ec39ebc4351641a510f5532346
Itrukhin <5526173+itrukhin@users.noreply.github.com>
2020-11-02 00:12:31 +0300

    alpha version

---
+++ src/Monolog/ExceptionHandlerLog.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Monolog;
+
+use App\Log;
+use Monolog\Logger;
+
+class ExceptionHandlerLog extends \Bitrix\Main\Diag\ExceptionHandlerLog {
+
+    /**
+     * @var Logger
+     */
+	protected $logger;
+	/**
+     * @var callable
+     */
+	protected $context;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize(array $options)
+    {
+        if (isset($options['context']) && is_callable($options['context']))
+        {
+            $this->context = $options['context'];
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($exception, $logType)
+    {
+        try {
+            $log = new Log();
+            if(is_callable($this->context)) {
+                try {
+                    $context = call_user_func($this->context, $exception);
+                } catch(\Exception $e) {
+                    $log->logInnerException(new \Exception('Can not call ' . $this->context));
+                }
+            }
+            $log->critical($exception, (array) $this->context);
+        } catch(\Exception $e) {
+            $log->logInnerException($e);
+        }
+    }
+}
