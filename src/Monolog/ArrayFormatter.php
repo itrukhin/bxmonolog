e47156902364c8833de5cab4241cac8aa10e6393
Ioannes <itrukhin@gmail.com>
2022-08-04 23:43:26 +0300

    fix multidimensional context

--- src/Monolog/ArrayFormatter.php
+++ src/Monolog/ArrayFormatter.php
@@ -15,11 +15,7 @@ class ArrayFormatter extends NormalizerFormatter
 		$output = array(sprintf("%s %s", $record['level_name'], $date->format("d.m.Y H:i:s")));
         $output[] = $record['message'];
 		if(is_array($record['context'])) {
-		    if(count($record['context']) == 1) {
-                $output[] = current($record['context']);
-            } else if(count($record['context']) > 1) {
-                $output[] = print_r($record['context'], true);
-            }
+            $output[] = print_r($record['context'], true);
         } else if(!empty($record['context'])) {
             $output[] = $record['context'];
         }
